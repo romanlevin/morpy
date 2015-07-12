8f8b3551792dad4a456f9ed1872e0f05ce3cd2b8
Roman Levin <romanlevin@gmail.com>
2015-07-12 11:14:37 +0200

    Draw all boards from sparse positions
    Experimenting in generating complete position and boards using
    sparse position modelling.

--- chess.py
+++ chess.py
@@ -1,4 +1,5 @@
 import itertools
+import textwrap
 
 PIECES = {
     'K': 'rules',
@@ -17,5 +18,24 @@ def get_positions(x, y, pieces):
     coordinates = get_coordinates(x, y)
     return itertools.permutations(coordinates, pieces)
 
+
+def get_boards(x, y, pieces=''):
+    pieces = sorted(pieces)
+    positions = get_positions(x, y, len(pieces))
+    for position in positions:
+        board = {
+            coordinate: piece for coordinate, piece in zip(position, pieces)}
+        yield board
+
+
+def draw_board(board, x, y):
+    array = ''.join(
+        board.get(coordinate, '□') for coordinate in get_coordinates(x, y))
+    print(textwrap.fill(array, x, drop_whitespace=False))
+
 # print(tuple(get_coordinates(2, 2)))
-print(tuple(get_positions(2, 2, 1)))
+# print(tuple(get_positions(2, 2, 1)))
+X, Y, PIECES = 2, 2, '♕'
+for board in get_boards(X, Y, PIECES):
+    draw_board(board, X, Y)
+    print('-' * X)
